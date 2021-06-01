b56fc761c2b9c240b15cf9f0d68664d5b9ae8551
vsosahdz <darklink@DESKTOP-PVL9G23.localdomain>
2021-06-01 01:00:45 -0500

    Nuevo contenido

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+//Importar la biblioteca express para la creación de servidores
+const express = require('express');
 
+//Crear el servidor
+const app = express();
+
+//Respuesta al método GET con un mensaje de texto
+app.get('/',(req,res)=>{
+    res.send('Hola que tal bienvenido');
+});
+
+app.listen(80,()=>console.log("Servidor HTTP en línea en el puerto 80"));
